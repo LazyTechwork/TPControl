228b3c478bfcda44947eb456df301650fb1b61dd
LazyTechwork <lazytechwork@gmail.com>
2020-03-04 23:01:37 +0300

    Advancement triggers

---
+++ src/main/java/ru/lazytechwork/tpcontrol/triggers/ModTriggers.java
@@ -0,0 +1,7 @@
+package ru.lazytechwork.tpcontrol.triggers;
+
+public class ModTriggers {
+    public static final TeleportTrigger TELEPORT_TRIGGER = new TeleportTrigger();
+
+    public static final TeleportTrigger[] TRIGGERS = new TeleportTrigger[]{TELEPORT_TRIGGER};
+}
