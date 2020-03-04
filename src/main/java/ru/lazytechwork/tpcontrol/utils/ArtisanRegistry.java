b079843c94ec343d2acdb275258cef357310ca01
LazyTechwork <lazytechwork@gmail.com>
2020-03-04 23:48:58 +0300

    Fixed advancement trigger registry

--- src/main/java/ru/lazytechwork/tpcontrol/utils/ArtisanRegistry.java
+++ src/main/java/ru/lazytechwork/tpcontrol/utils/ArtisanRegistry.java
@@ -3,7 +3,7 @@ package ru.lazytechwork.tpcontrol.utils;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.advancements.ICriterionInstance;
 import net.minecraft.advancements.ICriterionTrigger;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -14,7 +14,7 @@ public class ArtisanRegistry {
     @SuppressWarnings("unchecked")
     public static <T extends ICriterionInstance> ICriterionTrigger<T> registerAdvancementTrigger(ICriterionTrigger<T> trigger) {
         if (CriterionRegister == null) {
-            CriterionRegister = ObfuscationReflectionHelper.findMethod(CriteriaTriggers.class, "register", ICriterionTrigger.class);
+            CriterionRegister = ReflectionHelper.findMethod(CriteriaTriggers.class, "register", "func_192118_a", ICriterionTrigger.class);
             CriterionRegister.setAccessible(true);
         }
         try {
