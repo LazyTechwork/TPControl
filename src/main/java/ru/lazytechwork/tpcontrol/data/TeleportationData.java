a6e6752c9de602f6c8113456d463dd682c96b65a
LazyTechwork <lazytechwork@gmail.com>
2020-03-04 20:48:00 +0300

    Some changes

--- src/main/java/ru/lazytechwork/tpcontrol/data/TeleportationData.java
+++ src/main/java/ru/lazytechwork/tpcontrol/data/TeleportationData.java
@@ -17,15 +17,16 @@ public class TeleportationData extends WorldSavedData {
 
     public TeleportationData() {
         super(DATA_NAME);
+        markDirty();
     }
 
     public static TeleportationData get(World world) {
         MapStorage storage = world.getMapStorage();
+        assert storage != null;
         TeleportationData data = (TeleportationData) storage.getOrLoadData(TeleportationData.class, DATA_NAME);
         if (data == null) {
             data = new TeleportationData();
             data.teleportCounts = new HashMap<String, Integer>();
-            data.markDirty();
             storage.setData(DATA_NAME, data);
         }
         return data;
@@ -33,21 +34,22 @@ public class TeleportationData extends WorldSavedData {
 
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
+        TPControl.LOGGER.info("readFromNBT");
         if (nbt.hasKey(DATA_NAME)) {
             String[] data = nbt.getString(DATA_NAME).split(";");
             for (int i = 0; i < data.length; i++) {
                 String[] info = data[i].split(":");
+                TPControl.LOGGER.info(info[0] + " - " + Integer.parseInt(info[1]));
                 teleportCounts.put(info[0], Integer.parseInt(info[1]));
             }
+            markDirty();
         }
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        ArrayList<String> resultString = new ArrayList<String>();
-        for (Map.Entry<String, Integer> entry : teleportCounts.entrySet())
-            resultString.add(entry.getKey() + ":" + entry.getValue());
-        compound.setString(DATA_NAME, String.join(";", resultString));
+        TPControl.LOGGER.info("writeToNBT");
+        compound.setString(DATA_NAME, toString());
         return compound;
     }
 
@@ -57,7 +59,7 @@ public class TeleportationData extends WorldSavedData {
 
     public void setTeleportCounts(HashMap<String, Integer> teleportCounts) {
         this.teleportCounts = teleportCounts;
-        this.markDirty();
+        markDirty();
     }
 
     @Override
--- src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
+++ src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
@@ -2,10 +2,8 @@ package ru.lazytechwork.tpcontrol.events;
 
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import ru.lazytechwork.tpcontrol.TPControl;
 import ru.lazytechwork.tpcontrol.data.TeleportationData;
 
 import java.util.HashMap;
@@ -15,8 +13,6 @@ public class EventsHandler {
     @SubscribeEvent
     public void onCommand(CommandEvent event) {
         ICommandSender sender = event.getSender();
-        sender.sendMessage(new TextComponentString(
-                event.getCommand().getName() + " " + event.getParameters()[0] + " " + event.getParameters()[1]));
         String command = event.getCommand().getName();
         if (!Objects.equals(command, "tp"))
             return;
@@ -24,19 +20,15 @@ public class EventsHandler {
         TeleportationData tpdata = TeleportationData.get(sender.getEntityWorld());
         HashMap<String, Integer> data = tpdata.getTeleportCounts();
         if (pars.length == 1) {
-            TPControl.LOGGER.info(sender.getName());
-            TPControl.LOGGER.info(sender.getDisplayName());
-            if (Objects.equals(pars[0], sender.getName()))
-                return;
+//            if (Objects.equals(pars[0], sender.getName()))
+//                return;
             data.put(pars[0], data.getOrDefault(pars[0], 0) + 1);
         } else if (pars.length == 2) {
-            TPControl.LOGGER.info(sender.getName());
-            TPControl.LOGGER.info(sender.getDisplayName());
-            if (Objects.equals(pars[0], pars[1]))
-                return;
+//            if (Objects.equals(pars[0], pars[1]))
+//                return;
             data.put(pars[1], data.getOrDefault(pars[1], 0) + 1);
         }
         tpdata.setTeleportCounts(data);
-        sender.sendMessage(new TextComponentString(TextFormatting.GOLD + tpdata.toString()));
+        sender.sendMessage(new TextComponentString(tpdata.toString()));
     }
 }
