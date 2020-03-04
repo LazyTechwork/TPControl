e752a48ae392a2588dfbe1e6a9f5f55798e19800
LazyTechwork <lazytechwork@gmail.com>
2020-03-04 19:03:42 +0300

    Handler check

--- src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
+++ src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
@@ -1,13 +1,12 @@
 package ru.lazytechwork.tpcontrol.events;
 
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import ru.lazytechwork.tpcontrol.TPControl;
 
 public class EventsHandler {
     @SubscribeEvent
     public void onCommand(CommandEvent event) {
-        TPControl.LOGGER.info(event.getCommand().getName());
-        TPControl.LOGGER.info(event.getParameters());
+        event.getSender().sendMessage(new TextComponentString(event.getCommand().getName() + " " + event.getParameters()[0] + " " + event.getParameters()[1]));
     }
 }
