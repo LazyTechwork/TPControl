31446e098a8ab8c5d170c1c971be6d6ad61e52f7
LazyTechwork <lazytechwork@gmail.com>
2020-03-04 19:58:41 +0300

    Added command processor

--- src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
+++ src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
@@ -5,25 +5,34 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import ru.lazytechwork.tpcontrol.TPControl;
-import ru.lazytechwork.tpcontrol.data.TeleportCount;
 import ru.lazytechwork.tpcontrol.data.TeleportationData;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class EventsHandler {
     @SubscribeEvent
     public void onCommand(CommandEvent event) {
-        event.getSender().sendMessage(new TextComponentString(event.getCommand().getName() + " " + event.getParameters()[0] + " " + event.getParameters()[1]));
+        event.getSender().sendMessage(new TextComponentString(
+                event.getCommand().getName() + " " + event.getParameters()[0] + " " + event.getParameters()[1]));
         ICommandSender sender = event.getSender();
         String command = event.getCommand().getName();
         String pars[] = event.getParameters();
-        List<TeleportCount> data = TeleportationData.get(sender.getEntityWorld()).getTeleportCounts();
+        TeleportationData tpdata = TeleportationData.get(sender.getEntityWorld());
+        HashMap<String, Integer> data = tpdata.getTeleportCounts();
         if (pars.length == 1) {
             TPControl.LOGGER.info(sender.getName());
             TPControl.LOGGER.info(sender.getDisplayName());
             if (Objects.equals(pars[0], sender.getName()))
                 return;
+            data.put(pars[0], data.getOrDefault(pars[0], 0) + 1);
+        } else if (pars.length == 2) {
+            TPControl.LOGGER.info(sender.getName());
+            TPControl.LOGGER.info(sender.getDisplayName());
+            if (Objects.equals(pars[0], pars[1]))
+                return;
+            data.put(pars[1], data.getOrDefault(pars[1], 0) + 1);
         }
+        tpdata.setTeleportCounts(data);
     }
 }
