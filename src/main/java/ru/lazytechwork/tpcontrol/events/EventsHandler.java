13989b98832a4c22786471609b1d3c9dbdf539f3
LazyTechwork <lazytechwork@gmail.com>
2020-03-04 20:09:25 +0300

    Added debugger

--- src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
+++ src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
@@ -2,6 +2,7 @@ package ru.lazytechwork.tpcontrol.events;
 
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import ru.lazytechwork.tpcontrol.TPControl;
@@ -13,10 +14,12 @@ import java.util.Objects;
 public class EventsHandler {
     @SubscribeEvent
     public void onCommand(CommandEvent event) {
-        event.getSender().sendMessage(new TextComponentString(
-                event.getCommand().getName() + " " + event.getParameters()[0] + " " + event.getParameters()[1]));
         ICommandSender sender = event.getSender();
+        sender.sendMessage(new TextComponentString(
+                event.getCommand().getName() + " " + event.getParameters()[0] + " " + event.getParameters()[1]));
         String command = event.getCommand().getName();
+        if (!Objects.equals(command, "tp"))
+            return;
         String pars[] = event.getParameters();
         TeleportationData tpdata = TeleportationData.get(sender.getEntityWorld());
         HashMap<String, Integer> data = tpdata.getTeleportCounts();
@@ -34,5 +37,6 @@ public class EventsHandler {
             data.put(pars[1], data.getOrDefault(pars[1], 0) + 1);
         }
         tpdata.setTeleportCounts(data);
+        sender.sendMessage(new TextComponentString(TextFormatting.GOLD + tpdata.toString()));
     }
 }
