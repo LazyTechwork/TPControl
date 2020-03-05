f7edb08f2c5dc9770b9b4474e4e7982a8d391eea
LazyTechwork <lazytechwork@gmail.com>
2020-03-05 18:13:17 +0300

    Changed structure of EventsHandler

--- src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
+++ src/main/java/ru/lazytechwork/tpcontrol/events/EventsHandler.java
@@ -8,8 +8,8 @@ import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import ru.lazytechwork.tpcontrol.TPControl;
-import ru.lazytechwork.tpcontrol.data.TeleportationData;
 import ru.lazytechwork.tpcontrol.advancements.AdvancementManager;
+import ru.lazytechwork.tpcontrol.data.TeleportationData;
 
 import java.util.HashMap;
 import java.util.Objects;
@@ -27,19 +27,24 @@ public class EventsHandler {
         String pars[] = event.getParameters();
         TeleportationData tpdata = TeleportationData.get(sender.getEntityWorld());
         HashMap<String, Integer> data = tpdata.getTeleportCounts();
+        String key = null;
+        int tpcount = -1;
         if (pars.length == 1) {
 //            if (Objects.equals(pars[0], sender.getName()))
 //                return;
-            data.put(pars[0], data.getOrDefault(pars[0], 0) + 1);
-            AdvancementManager.TELEPORT_TRIGGER.trigger((EntityPlayerMP) Objects.requireNonNull(sender.getCommandSenderEntity()), data.get(pars[0]));
+            key = pars[0];
+            tpcount = data.getOrDefault(pars[0], 0) + 1;
         } else if (pars.length == 2) {
 //            if (Objects.equals(pars[0], pars[1]))
 //                return;
-            data.put(pars[1], data.getOrDefault(pars[1], 0) + 1);
-            AdvancementManager.TELEPORT_TRIGGER.trigger((EntityPlayerMP) Objects.requireNonNull(sender.getCommandSenderEntity()), data.get(pars[1]));
+            key = pars[1];
+            tpcount = data.getOrDefault(pars[1], 0) + 1;
         }
+        if (key == null)
+            return;
+        data.put(key, tpcount);
         tpdata.setTeleportCounts(data);
-        event.getSender().getServer().getAdvancementManager().reload();
+        AdvancementManager.TELEPORT_TRIGGER.trigger((EntityPlayerMP) sender.getCommandSenderEntity(), tpcount);
         event.getSender().getServer().getAdvancementManager().getAdvancements().forEach((Advancement advancement) -> TPControl.LOGGER.info(advancement.getId()));
         sender.sendMessage(new TextComponentString(tpdata.toString()));
     }
